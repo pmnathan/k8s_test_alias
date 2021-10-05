bf8adf3622d8b65254062128d1ec23c21ba6415e
Prakash Manglanathan <pmnathan@gmail.com>
2021-10-05 09:00:33 -0400

    Update main.tf

--- aks-cluster/main.tf
+++ aks-cluster/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   dns_prefix          = var.cluster_name
 
   default_node_pool {
-    name       = "test"
+    name       = "prakash"
     node_count = 2
     vm_size    = "Standard_DS2_v2"
   }
